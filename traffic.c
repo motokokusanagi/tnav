faec84caab5ba74c5000ece48bc841451d9f6498
Arthur Fayt <hexlotar@hell.(none)>
2011-12-07 17:08:38 +0200

    added query function

--- traffic.c
+++ traffic.c
@@ -34,6 +34,7 @@
 #include "transform.h"
 #include "file.h"
 
+#include <dbus/dbus.h>
 #include "traffic.h"
 
 static int map_id;
@@ -377,7 +378,6 @@ map_new_traffic(struct map_methods *meth, struct attr **attrs, struct callback_l
 	wdata=g_strdup(data->u.str); // strings points to file of map
 	//dbg(0,"data %s\n",data->u.str);
 	len=strlen(wdata); 
-	
 	// pipes will be not used
 	if (len && wdata[len-1] == '|') {
 		wdata[len-1]='\0';
@@ -386,36 +386,136 @@ map_new_traffic(struct map_methods *meth, struct attr **attrs, struct callback_l
 	wexp=file_wordexp_new(wdata);
 	wexp_data=file_wordexp_get_array(wexp);
 	*meth=map_methods_traffic;
-
 	m=g_new0(struct map_priv, 1);
 	m->id=++map_id; // will be there
 	//m->filename=g_strdup(wexp_data[0]); // file name will has gone
 	m->is_pipe=is_pipe; // will has gone
 	if (flags)  
 		m->flags=flags->u.num; // 
-	
-	
 	// счас здесь создадим файлик с содержимым
+	//
 	char *fname = "./hello_my.txt";
 	char *fmode = "w";
+	m->filename = g_strdup(fname);
 	FILE * fil = fopen(fname,fmode);
-	m->filename = fname;
 	fprintf(fil,"type=street_traffic\n");
-	
 	fprintf(fil,"4629.868000 N 3037.662000 E\n");
 	fprintf(fil,"4623.916000 N 3046.296000 E\n");
-	fprintf(fil,"4628.916000 N 3081.296000 E\n");
-	fprintf(fil,"4624.916000 N 3046.296000 E\n");
+	//fprintf(fil,"4628.916000 N 3081.296000 E\n");
+	//fprintf(fil,"4624.916000 N 3046.296000 E\n");
 	fclose(fil);
-	dbg(0,"map_new_traffic %s %s\n", m->filename, wdata);
+	dbg(0,"map_new_traffic %s\n", m->filename);
 	if (charset) {
 		m->charset=g_strdup(charset->u.str);
 		meth->charset=m->charset;
 	}
 	file_wordexp_destroy(wexp);
 	g_free(wdata);
-	
 	return m;
+}
+
+
+void query(void) 
+{
+   DBusMessage* msg;
+   DBusMessageIter args;
+   DBusConnection* conn;
+   DBusError err;
+   DBusPendingCall* pending;
+ //  int ret;
+   bool stat;
+   dbus_uint32_t level;
+
+   printf("Calling remote method with %s\n", param);
+
+   // initialiset the errors
+   dbus_error_init(&err);
+
+   // connect to the system bus and check for errors
+   conn = dbus_bus_get(DBUS_BUS_SESSION, &err);
+   if (dbus_error_is_set(&err)) { 
+      fprintf(stderr, "Connection Error (%s)\n", err.message); 
+      dbus_error_free(&err);
+   }
+   if (NULL == conn) { 
+      exit(1); 
+   }
+
+   // request our name on the bus
+   ret = dbus_bus_request_name(conn, "test.method.caller", DBUS_NAME_FLAG_REPLACE_EXISTING , &err);
+   if (dbus_error_is_set(&err)) { 
+      fprintf(stderr, "Name Error (%s)\n", err.message); 
+      dbus_error_free(&err);
+   }
+   if (DBUS_REQUEST_NAME_REPLY_PRIMARY_OWNER != ret) { 
+      exit(1);
+   }
+
+   // create a new method call and check for errors
+   msg = dbus_message_new_method_call("test.method.server", // target for the method call
+                                      "/test/method/Object", // object to call on
+                                      "test.method.Type", // interface to call on
+                                      "Method"); // method name
+   if (NULL == msg) { 
+      fprintf(stderr, "Message Null\n");
+      exit(1);
+   }
+
+   // append arguments
+   dbus_message_iter_init_append(msg, &args);
+   if (!dbus_message_iter_append_basic(&args, DBUS_TYPE_STRING, &param)) {
+      fprintf(stderr, "Out Of Memory!\n"); 
+      exit(1);
+   }
+   
+   // send message and get a handle for a reply
+   if (!dbus_connection_send_with_reply (conn, msg, &pending, -1)) { // -1 is default timeout
+      fprintf(stderr, "Out Of Memory!\n"); 
+      exit(1);
+   }
+   if (NULL == pending) { 
+      fprintf(stderr, "Pending Call Null\n"); 
+      exit(1); 
+   }
+   dbus_connection_flush(conn);
+   
+   printf("Request Sent\n");
+   
+   // free message
+   dbus_message_unref(msg);
+   
+   // block until we recieve a reply
+   dbus_pending_call_block(pending);
+
+   // get the reply message
+   msg = dbus_pending_call_steal_reply(pending);
+   if (NULL == msg) {
+      fprintf(stderr, "Reply Null\n"); 
+      exit(1); 
+   }
+   // free the pending message handle
+   dbus_pending_call_unref(pending);
+
+   // read the parameters
+   if (!dbus_message_iter_init(msg, &args))
+      fprintf(stderr, "Message has no arguments!\n"); 
+   else if (DBUS_TYPE_BOOLEAN != dbus_message_iter_get_arg_type(&args)) 
+      fprintf(stderr, "Argument is not boolean!\n"); 
+   else
+      dbus_message_iter_get_basic(&args, &stat);
+
+   if (!dbus_message_iter_next(&args))
+      fprintf(stderr, "Message has too few arguments!\n"); 
+   else if (DBUS_TYPE_UINT32 != dbus_message_iter_get_arg_type(&args)) 
+      fprintf(stderr, "Argument is not int!\n"); 
+   else
+      dbus_message_iter_get_basic(&args, &level);
+
+   printf("Got Reply: %d, %d\n", stat, level);
+   
+   // free reply and close connection
+   dbus_message_unref(msg);   
+   dbus_connection_close(conn);
 }
 
 void
