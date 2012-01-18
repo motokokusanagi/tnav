0958addfcdcdd2d9ee842909a3ec8323f2fd3efb
Jack <jack@jack-desktop.(none)>
2012-01-18 16:37:31 +0200

    query

--- traffic.c
+++ traffic.c
@@ -35,8 +35,7 @@
 #include "attr.h"
 #include "transform.h"
 #include "file.h"
-#include <json/json.h>
-#include <dbus/dbus.h>
+//#include <dbus/dbus.h>
 #include "traffic.h"
 
 static int map_id;
@@ -110,7 +109,7 @@ traffic_coord_get(void *priv_data, struct coord *c, int count)
 		} else {
 			mr->more=0;
 			break;
-		}
+
 	}
 	return ret;
 }
@@ -187,41 +186,44 @@ map_rect_new_traffic(struct map_priv *map, struct map_selection *sel)
 	mr->item.id_lo=0;
 	mr->item.meth=&methods_traffic; // too 
 	mr->item.priv_data=mr; //too
-	if (map->is_pipe) {
-#ifdef HAVE_POPEN
-
-		char *oargs,*args=g_strdup(map->filename),*sep=" ";
-		enum layer_type lay;
-		g_free(mr->args);
-		while (sel) {
-			oargs=args;
-			args=g_strdup_printf("%s 0x%x 0x%x 0x%x 0x%x", oargs, sel->u.c_rect.lu.x, sel->u.c_rect.lu.y, sel->u.c_rect.rl.x, sel->u.c_rect.rl.y);
-			g_free(oargs);
-			for (lay=layer_town ; lay < layer_end ; lay++) {
-				oargs=args;
-				args=g_strdup_printf("%s%s%d", oargs, sep, sel->order);
-				g_free(oargs);
-				sep=",";
-			}
-			sel=sel->next;
-		}
-		dbg(1,"popen args %s\n", args);
-		mr->args=args;
-		mr->f=popen(mr->args, "r");
-		mr->pos=0;
-		mr->lastlen=0;
-#else
-		dbg(0,"map_rect_new_traffic is unable to work with pipes %s\n",map->filename);
-#endif 
+	mr->traffic_list=NULL;
+	query (mr->traffic_list);
 	
-	  
-	} else {
-		mr->f=fopen(map->filename, "r");
-	}
-	if(!mr->f) {
-		printf("map_rect_new_traffic unable to open traffic %s. Error: %s\n",map->filename, strerror(errno));
-	}
-	get_line(mr);
+//	if (map->is_pipe) {
+//#ifdef HAVE_POPEN
+//
+//		char *oargs,*args=g_strdup(map->filename),*sep=" ";
+//		enum layer_type lay;
+//		g_free(mr->args);
+//		while (sel) {
+//			oargs=args;
+//			args=g_strdup_printf("%s 0x%x 0x%x 0x%x 0x%x", oargs, sel->u.c_rect.lu.x, sel->u.c_rect.lu.y, sel->u.c_rect.rl.x, sel->u.c_rect.rl.y);
+//			g_free(oargs);
+//			for (lay=layer_town ; lay < layer_end ; lay++) {
+//				oargs=args;
+//				args=g_strdup_printf("%s%s%d", oargs, sep, sel->order);
+//				g_free(oargs);
+//				sep=",";
+//			}
+//			sel=sel->next;
+//		}
+//		dbg(1,"popen args %s\n", args);
+//		mr->args=args;
+//		mr->f=popen(mr->args, "r");
+//		mr->pos=0;
+//		mr->lastlen=0;
+//#else
+//		dbg(0,"map_rect_new_traffic is unable to work with pipes %s\n",map->filename);
+//#endif
+//
+//
+//	} else {
+//		mr->f=fopen(map->filename, "r");
+//	}
+//	if(!mr->f) {
+//		printf("map_rect_new_traffic unable to open traffic %s. Error: %s\n",map->filename, strerror(errno));
+//	}
+//	get_line(mr);
 	return mr;
 }
 
@@ -229,98 +231,99 @@ map_rect_new_traffic(struct map_priv *map, struct map_selection *sel)
 static void
 map_rect_destroy_traffic(struct map_rect_priv *mr)
 {
-	if (mr->f) {
-		if (mr->m->is_pipe) {
-#ifdef HAVE_POPEN
-			pclose(mr->f);
-#endif
-		}
-		else {
-			fclose(mr->f);
-		}
-	}
+//	if (mr->f) {
+//		if (mr->m->is_pipe) {
+//#ifdef HAVE_POPEN
+//			pclose(mr->f);
+//#endif
+//		}
+//		else {
+//			fclose(mr->f);
+//		}
+//	}
+	g_list_free (mr->traffic_list);
         g_free(mr);
 }
 
 static struct item *
 map_rect_get_item_traffic(struct map_rect_priv *mr)
 {
-	printf("\n%p",&mr->item);
-	char *p,type[SIZE];
-	dbg(1,"map_rect_get_item_traffic id_hi=%d line=%s\n", mr->item.id_hi, mr->line);
-	if (!mr->f) {
-		return NULL;
-	}
-	if (mr->more != 0) printf("\n%d",mr->more);
-	//more is special element for i
-	//while (mr->more) {
-		//struct coord c;
-		//traffic_coord_get(mr, &c, 1);
-	//}
-	for(;;) {
-		if (feof(mr->f)) {
-			dbg(1,"map_rect_get_item_traffic: eof %d\n",mr->item.id_hi);
-			if (mr->m->flags & 1) {
-				if (!mr->item.id_hi) 
-					return NULL;
-				mr->item.id_hi=0;
-			} else {
-				if (mr->item.id_hi) 
-					return NULL;
-				mr->item.id_hi=1;
-			}
-			if (mr->m->is_pipe) {
-#ifdef HAVE_POPEN
-				pclose(mr->f);
-				mr->f=popen(mr->args, "r");
-				mr->pos=0;
-				mr->lastlen=0;
-#endif
-			} else {
-				fseek(mr->f, 0, SEEK_SET);
-				clearerr(mr->f);
-			}
-			get_line(mr);
-		}
-		if ((p=strchr(mr->line,'\n'))) 
-			*p='\0';
-		if (mr->item.id_hi) {
-			mr->attrs[0]='\0';
-			if (!parse_line(mr, 1)) {
-				get_line(mr);
-				continue;
-			}
-			dbg(1,"map_rect_get_item_traffic: point found\n");
-			mr->eoc=0;
-			mr->item.id_lo=mr->pos;
-		} else {
-			if (parse_line(mr, 1)) {
-				get_line(mr);
-				continue;
-			}
-			dbg(1,"map_rect_get_item_traffic: line found\n");
-			if (! mr->line[0]) {
-				get_line(mr);
-				continue;
-			}
-			mr->item.id_lo=mr->pos;
-			strcpy(mr->attrs, mr->line);
-			get_line(mr);
-			dbg(1,"mr=%p attrs=%s\n", mr, mr->attrs);
-		}
-		dbg(1,"get_attrs %s\n", mr->attrs);
-		if (attr_from_line(mr->attrs,"type",NULL,type,NULL)) {
-			dbg(1,"type='%s'\n", type);
-			mr->item.type=item_from_name(type);
-			if (mr->item.type == type_none) 
-				printf("Warning: type '%s' unknown\n", type);
-		} else {
-			get_line(mr);
-			continue;
-		}
-		mr->attr_last=attr_none;
-		mr->more=1;
-		dbg(1,"return attr='%s'\n", mr->attrs);
+//	printf("\n%p",&mr->item);
+//	char *p,type[SIZE];
+//	dbg(1,"map_rect_get_item_traffic id_hi=%d line=%s\n", mr->item.id_hi, mr->line);
+//	if (!mr->f) {
+//		return NULL;
+//	}
+//	if (mr->more != 0) printf("\n%d",mr->more);
+//	//more is special element for i
+//	//while (mr->more) {
+//		//struct coord c;
+//		//traffic_coord_get(mr, &c, 1);
+//	//}
+//	for(;;) {
+//		if (feof(mr->f)) {
+//			dbg(1,"map_rect_get_item_traffic: eof %d\n",mr->item.id_hi);
+//			if (mr->m->flags & 1) {
+//				if (!mr->item.id_hi)
+//					return NULL;
+//				mr->item.id_hi=0;
+//			} else {
+//				if (mr->item.id_hi)
+//					return NULL;
+//				mr->item.id_hi=1;
+//			}
+//			if (mr->m->is_pipe) {
+//#ifdef HAVE_POPEN
+//				pclose(mr->f);
+//				mr->f=popen(mr->args, "r");
+//				mr->pos=0;
+//				mr->lastlen=0;
+//#endif
+//			} else {
+//				fseek(mr->f, 0, SEEK_SET);
+//				clearerr(mr->f);
+//			}
+//			get_line(mr);
+//		}
+//		if ((p=strchr(mr->line,'\n')))
+//			*p='\0';
+//		if (mr->item.id_hi) {
+//			mr->attrs[0]='\0';
+//			if (!parse_line(mr, 1)) {
+//				get_line(mr);
+//				continue;
+//			}
+//			dbg(1,"map_rect_get_item_traffic: point found\n");
+//			mr->eoc=0;
+//			mr->item.id_lo=mr->pos;
+//		} else {
+//			if (parse_line(mr, 1)) {
+//				get_line(mr);
+//				continue;
+//			}
+//			dbg(1,"map_rect_get_item_traffic: line found\n");
+//			if (! mr->line[0]) {
+//				get_line(mr);
+//				continue;
+//			}
+//			mr->item.id_lo=mr->pos;
+//			strcpy(mr->attrs, mr->line);
+//			get_line(mr);
+//			dbg(1,"mr=%p attrs=%s\n", mr, mr->attrs);
+//		}
+//		dbg(1,"get_attrs %s\n", mr->attrs);
+//		if (attr_from_line(mr->attrs,"type",NULL,type,NULL)) {
+//			dbg(1,"type='%s'\n", type);
+//			mr->item.type=item_from_name(type);
+//			if (mr->item.type == type_none)
+//				printf("Warning: type '%s' unknown\n", type);
+//		} else {
+//			get_line(mr);
+//			continue;
+//		}
+//		mr->attr_last=attr_none;
+//		mr->more=1;
+//		dbg(1,"return attr='%s'\n", mr->attrs);
 		return &mr->item;
 	}
 }
@@ -328,17 +331,17 @@ map_rect_get_item_traffic(struct map_rect_priv *mr)
 static struct item *
 map_rect_get_item_byid_traffic(struct map_rect_priv *mr, int id_hi, int id_lo)
 {
-	if (mr->m->is_pipe) {
-#ifndef _MSC_VER
-		pclose(mr->f);
-		mr->f=popen(mr->args, "r");
-		mr->pos=0;
-		mr->lastlen=0;
-#endif /* _MSC_VER */
-	} else
-		fseek(mr->f, id_lo, SEEK_SET);
-	get_line(mr);
-	mr->item.id_hi=id_hi;
+//	if (mr->m->is_pipe) {
+//#ifndef _MSC_VER
+//		pclose(mr->f);
+//		mr->f=popen(mr->args, "r");
+//		mr->pos=0;
+//		mr->lastlen=0;
+//#endif /* _MSC_VER */
+//	} else
+//		fseek(mr->f, id_lo, SEEK_SET);
+//	get_line(mr);
+//	mr->item.id_hi=id_hi;
 	return map_rect_get_item_traffic(mr);
 }
 
@@ -352,23 +355,7 @@ static struct map_methods map_methods_traffic = {
 	map_rect_get_item_byid_traffic,
 };
 
-/*struct TraffCoord
-{
-	double FirstCoord, SecondCoord;
-	char Direction[2];
-	//int MaxSpeed;
-} traf[100] = {  {4629.868000,3037.662000,4623.916000,3046.296000,'N','E','N','E',5}
-  
-};*/
 
-
-
-struct TraffCoord
-{
-	double FirstCoord, SecondCoord, ThirdCoord, FourthCoord;
-	char Direction[4];
-	int MaxSpeed;
-};
 
 static struct map_priv *
 map_new_traffic(struct map_methods *meth, struct attr **attrs, struct callback_list *cbl)
@@ -489,111 +476,137 @@ int  ParseJsonData (struct TraffCoord *TraffData, char * strJson)
 */
 
 //void query(char *str)
-void query(struct TraffCoord *traf,int *count)
+void query(GList *traffic_list)
 {
-   DBusMessage* msg;
-   DBusMessageIter args;
-   DBusConnection* conn;
-   DBusError err;
-   DBusPendingCall* pending;
-   int ret;
-   char *stat;
-   char *param = "get";
-   //printf("Calling remote method with %s\n", param);
-   // initialiset the errors
-   dbus_error_init(&err);
-   // connect to the system bus and check for errors
-   conn = dbus_bus_get(DBUS_BUS_SESSION, &err);
-   if (dbus_error_is_set(&err)) { 
-      fprintf(stderr, "Connection Error (%s)\n", err.message); 
-      dbus_error_free(&err);
-   }
-   if (NULL == conn) { 
-      exit(1); 
-   }
+	traffic_item item_1 = (struct traffic_item *)malloc(sizeof(struct traffic_item));
+	item_1.x1=4629.868000;
+	item_1.y1=3037.662000;
+	item_1.x2=4624.916000;
+	item_1.y2=3046.296000;
+	item_1.sn1='N';
+	item_1.sn2='N';
+    item_1.ew1='E';
+    item_1.ew2='E';
+    item_1.speed=0.0;
 
-   // request our name on the bus
-   ret = dbus_bus_request_name(conn, "traffic.method.caller", DBUS_NAME_FLAG_REPLACE_EXISTING , &err);
-   if (dbus_error_is_set(&err)) { 
-      fprintf(stderr, "Name Error (%s)\n", err.message); 
-      dbus_error_free(&err);
-   }
-   if (DBUS_REQUEST_NAME_REPLY_PRIMARY_OWNER != ret) { 
-      exit(1);
-   }
+    traffic_item item_2 = (struct traffic_item *)malloc(sizeof(struct traffic_item));
+        item_2.x2=4623.916000;
+    	item_2.y2=3046.296000;
+    	item_2.x1=4624.916000;
+    	item_2.y1=3046.296000;
+    	item_2.sn1='N';
+    	item_2.sn2='N';
+        item_2.ew1='E';
+        item_2.ew2='E';
+        item_2.speed=0.0;
 
-   // create a new method call and check for errors
-   msg = dbus_message_new_method_call("traffic.method.server", // target for the method call
-                                      "/traffic/method/Object", // object to call on
-                                      "traffic.method.Type", // interface to call on
-                                      "Method"); // method name
-   if (NULL == msg) { 
-      fprintf(stderr, "Message Null\n");
-      exit(1);
-   }
+    traffic_list = g_list_append (traffic_list, item_1);
+    traffic_list = g_list_append (traffic_list, item_2);
 
-   // append arguments
-   dbus_message_iter_init_append(msg, &args);
-   if (!dbus_message_iter_append_basic(&args, DBUS_TYPE_STRING, &param)) {
-      fprintf(stderr, "Out Of Memory!\n"); 
-      exit(1);
-   }
-   
-   // send message and get a handle for a reply
-   if (!dbus_connection_send_with_reply (conn, msg, &pending, -1)) { // -1 is default timeout
-      fprintf(stderr, "Out Of Memory!\n"); 
-      exit(1);
-   }
-   if (NULL == pending) { 
-      fprintf(stderr, "Pending Call Null\n"); 
-      exit(1); 
-   }
-   dbus_connection_flush(conn);
-   
-   printf("Request Sent\n");
-   
-   // free message
-   dbus_message_unref(msg);
-   
-   // block until we recieve a reply
-   dbus_pending_call_block(pending);
+//   DBusMessage* msg;
+//   DBusMessageIter args;
+//   DBusConnection* conn;
+//   DBusError err;
+//   DBusPendingCall* pending;
+//   int ret;
+//   char *stat;
+//   char *param = "get";
+//   //printf("Calling remote method with %s\n", param);
+//   // initialiset the errors
+//   dbus_error_init(&err);
+//   // connect to the system bus and check for errors
+//   conn = dbus_bus_get(DBUS_BUS_SESSION, &err);
+//   if (dbus_error_is_set(&err)) {
+//      fprintf(stderr, "Connection Error (%s)\n", err.message);
+//      dbus_error_free(&err);
+//   }
+//   if (NULL == conn) {
+//      exit(1);
+//   }
+//
+//   // request our name on the bus
+//   ret = dbus_bus_request_name(conn, "traffic.method.caller", DBUS_NAME_FLAG_REPLACE_EXISTING , &err);
+//   if (dbus_error_is_set(&err)) {
+//      fprintf(stderr, "Name Error (%s)\n", err.message);
+//      dbus_error_free(&err);
+//   }
+//   if (DBUS_REQUEST_NAME_REPLY_PRIMARY_OWNER != ret) {
+//      exit(1);
+//   }
+//
+//   // create a new method call and check for errors
+//   msg = dbus_message_new_method_call("traffic.method.server", // target for the method call
+//                                      "/traffic/method/Object", // object to call on
+//                                      "traffic.method.Type", // interface to call on
+//                                      "Method"); // method name
+//   if (NULL == msg) {
+//      fprintf(stderr, "Message Null\n");
+//      exit(1);
+//   }
+//
+//   // append arguments
+//   dbus_message_iter_init_append(msg, &args);
+//   if (!dbus_message_iter_append_basic(&args, DBUS_TYPE_STRING, &param)) {
+//      fprintf(stderr, "Out Of Memory!\n");
+//      exit(1);
+//   }
+//
+//   // send message and get a handle for a reply
+//   if (!dbus_connection_send_with_reply (conn, msg, &pending, -1)) { // -1 is default timeout
+//      fprintf(stderr, "Out Of Memory!\n");
+//      exit(1);
+//   }
+//   if (NULL == pending) {
+//      fprintf(stderr, "Pending Call Null\n");
+//      exit(1);
+//   }
+//   dbus_connection_flush(conn);
+//
+//   printf("Request Sent\n");
+//
+//   // free message
+//   dbus_message_unref(msg);
+//
+//   // block until we recieve a reply
+//   dbus_pending_call_block(pending);
+//
+//   // get the reply message
+//   msg = dbus_pending_call_steal_reply(pending);
+//   if (NULL == msg) {
+//      fprintf(stderr, "Reply Null\n");
+//     // exit(1);
+//      goto l;
+//   }
+//   // free the pending message handle
+//   dbus_pending_call_unref(pending);
+//   // read the parameters
+//   if (!dbus_message_iter_init(msg, &args))
+//      fprintf(stderr, "Message has no arguments!\n");
+//   else if (DBUS_TYPE_STRING!= dbus_message_iter_get_arg_type(&args))
+//      fprintf(stderr, "Argument is not string!\n");
+//   else
+//      dbus_message_iter_get_basic(&args, &stat);
+//
+//
+// /// stat is json
+//
+//
+//   //strcpy(str,stat);
+//
+//  // printf("Got Reply: %s, \n", stat);
+//   l:
+//   // free reply and close connection
+//
+//   dbus_message_unref(msg);
+//   dbus_bus_release_name(conn,"traffic.method.caller",&err);
+//
+//   if(stat!=NULL){
+//      *count = ParseJsonData(traf,stat);
+//   } else {
+//
+//   }
+//   //dbus_connection_close(conn);
 
-   // get the reply message
-   msg = dbus_pending_call_steal_reply(pending);
-   if (NULL == msg) {
-      fprintf(stderr, "Reply Null\n"); 
-     // exit(1); 
-      goto l;
-   }
-   // free the pending message handle
-   dbus_pending_call_unref(pending);
-   // read the parameters
-   if (!dbus_message_iter_init(msg, &args))
-      fprintf(stderr, "Message has no arguments!\n"); 
-   else if (DBUS_TYPE_STRING!= dbus_message_iter_get_arg_type(&args)) 
-      fprintf(stderr, "Argument is not string!\n"); 
-   else
-      dbus_message_iter_get_basic(&args, &stat);
-   
-
- /// stat is json
-
-
-   //strcpy(str,stat);
-
-  // printf("Got Reply: %s, \n", stat);
-   l:
-   // free reply and close connection
-   
-   dbus_message_unref(msg);   
-   dbus_bus_release_name(conn,"traffic.method.caller",&err);
-
-   if(stat!=NULL){
-      *count = ParseJsonData(traf,stat);
-   } else {
-
-   }
-   //dbus_connection_close(conn);
 }
 
 void
