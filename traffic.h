f8de554440d955131e7a4bb6b114a2588eed2103
Arthur Fayt <hexlotar@gmail.com>
2012-01-19 11:51:10 +0200

    structure elemrnt added

--- traffic.h
+++ traffic.h
@@ -24,15 +24,13 @@
 
 typedef struct traffic_item
 {
-	double x1,x2,y1,y2,speed;
-	char ew1,sn1,ew2,sn2;
+	double x1,x2,y1,y2;
+	char ew1,sn1,ew2,sn2,speed;
 }traffic_item;
 
 struct map_priv {
 	int id;
-//	char *filename;
 	char *charset;
-	//int is_pipe;
 	int flags;
 };
 
@@ -40,22 +38,17 @@ struct map_priv {
 
 struct map_rect_priv {
 	struct map_selection *sel;
-  
-	//FILE *f;
 	GList *traffic_list;
 	long pos;///
-	char line[SIZE];///
+	char line[SIZE];
 	int attr_pos;
 	enum attr_type attr_last;
 	char attrs[SIZE];
 	char attr[SIZE];
 	char attr_name[SIZE];
 	struct coord c;
-	int eoc;
-	int more;
 	struct map_priv *m;
-	struct item item; // very 
+	struct item item;
 	char *args;
-	int lastlen;///
 };
 
